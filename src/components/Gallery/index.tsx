8eac00729afb8f75a1a5066051d38e6cc2cc0305
Leandro Sousa <156814218+leandrosousaljs@users.noreply.github.com>
2025-01-08 11:35:42 -0300

    Criando a galeria pt. I

---
+++ src/components/Gallery/index.tsx
@@ -0,0 +1,24 @@
+import zelda from '../../assets/images/banner-homem-aranha.png'
+import Section from '../Section'
+import { Item, Items } from './styles'
+
+const Gallery = () => (
+  <Section title="Galeria" background="black">
+    <Items>
+      <Item>
+        <img src={zelda} alt="imagem do link" />
+      </Item>
+      <Item>
+        <img src={zelda} alt="imagem do link" />
+      </Item>
+      <Item>
+        <img src={zelda} alt="imagem do link" />
+      </Item>
+      <Item>
+        <img src={zelda} alt="imagem do link" />
+      </Item>
+    </Items>
+  </Section>
+)
+
+export default Gallery
