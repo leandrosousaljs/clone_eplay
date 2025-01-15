77345627310f49ddeaf9432482641157c4f00a55
Leandro Sousa <156814218+leandrosousaljs@users.noreply.github.com>
2025-01-15 11:58:38 -0300

    Configurando o Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
-import Header from './components/Header'
-import { GlobalCss } from './styles'
-
+import { store } from './store'
 import Rotas from './routes'
+
 import Footer from './components/Footer'
+import Header from './components/Header'
+
+import { GlobalCss } from './styles'
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalCss />
-      <div className="container">
-        <Header />
-      </div>
-      <Rotas />
-      <Footer />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalCss />
+        <div className="container">
+          <Header />
+        </div>
+        <Rotas />
+        <Footer />
+      </BrowserRouter>
+    </Provider>
   )
 }
 
