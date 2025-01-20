fa91e80c6c7a714ea00677abde77cb6983ce31e5
Leandro Sousa <156814218+leandrosousaljs@users.noreply.github.com>
2025-01-20 11:44:07 -0300

    Fazendo requisições com o RTK Query

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,20 +1,13 @@
-import { useEffect, useState } from 'react'
+import { useGetFeaturedGameQuery } from '../../services/api'
 
 import { Imagem, Precos, Titulo } from './styles'
-
 import Tag from '../Tag'
 import Button from '../Button'
-import { Game } from '../../pages/Home'
+
 import { formataPreço } from '../ProductsList'
 
 const Banner = () => {
-  const [game, setGame] = useState<Game>()
-
-  useEffect(() => {
-    fetch('https://fake-api-tau.vercel.app/api/eplay/destaque').then((res) =>
-      res.json().then((res) => setGame(res))
-    )
-  }, [])
+  const { data: game, isLoading } = useGetFeaturedGameQuery()
 
   if (!game) {
     return <h3>Carregado...</h3>
