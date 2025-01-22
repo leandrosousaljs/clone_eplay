3b0372b0ac599964901790d5e08de94b28a4f5a3
Leandro Sousa <156814218+leandrosousaljs@users.noreply.github.com>
2025-01-22 14:56:24 -0300

    Adicionando e removendo produtos do carrinho

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,6 +1,9 @@
-import Button from '../Button'
+import { useDispatch, useSelector } from 'react-redux'
 
-import starWars from '../../assets/images/star_wars.png'
+import { RootReducer } from '../../store'
+import { close, remove } from '../../store/reducers/cart'
+
+import { formataPreço } from '../ProductsList'
 
 import {
   CartContainer,
@@ -10,43 +13,58 @@ import {
   Quantity,
   SideBar
 } from './styles'
+
+import Button from '../Button'
 import Tag from '../Tag'
 
-const Cart = () => (
-  <CartContainer>
-    <Overlay />
-    <SideBar>
-      <ul>
-        <CartItem>
-          <img src={starWars} alt="Star Wars" />
-          <div>
-            <h3>Nome do jogo</h3>
-            <Tag>RPG</Tag>
-            <Tag>PS5</Tag>
-            <span>R$ 150,00</span>
-          </div>
-          <button type="button" />
-        </CartItem>
-        <CartItem>
-          <img src={starWars} alt="Star Wars" />
-          <div>
-            <h3>Nome do jogo</h3>
-            <Tag>RPG</Tag>
-            <Tag>PS5</Tag>
-            <span>R$ 150,00</span>
-          </div>
-          <button type="button" />
-        </CartItem>
-      </ul>
-      <Quantity>2 jogo(s) no carrinho</Quantity>
-      <Prices>
-        Total de R$ 250,00 <span>Em até 6x sem juros</span>
-      </Prices>
-      <Button title="Clique aqui para continuar com a compra" type="button">
-        Continuar com a compra
-      </Button>
-    </SideBar>
-  </CartContainer>
-)
+const Cart = () => {
+  const { isOpen, items } = useSelector((state: RootReducer) => state.cart)
+
+  const dispatch = useDispatch()
+
+  const closeCart = () => {
+    dispatch(close())
+  }
+
+  const getTotalPrice = () => {
+    return items.reduce((acumulador, valorAtual) => {
+      return (acumulador += valorAtual.prices.current!)
+    }, 0)
+  }
+
+  const removeItem = (id: number) => {
+    dispatch(remove(id))
+  }
+
+  return (
+    <CartContainer className={isOpen ? 'is-open' : ''}>
+      <Overlay onClick={closeCart} />
+      <SideBar>
+        <ul>
+          {items.map((item) => (
+            <CartItem key={item.id}>
+              <img src={item.media.thumbnail} alt={item.name} />
+              <div>
+                <h3>{item.name}</h3>
+                <Tag>{item.details.category}</Tag>
+                <Tag>{item.details.system}</Tag>
+                <span>{formataPreço(item.prices.current)}</span>
+              </div>
+              <button onClick={() => removeItem(item.id)} type="button" />
+            </CartItem>
+          ))}
+        </ul>
+        <Quantity>{items.length} jogo(s) no carrinho</Quantity>
+        <Prices>
+          Total de {formataPreço(getTotalPrice())}{' '}
+          <span>Em até 6x sem juros</span>
+        </Prices>
+        <Button title="Clique aqui para continuar com a compra" type="button">
+          Continuar com a compra
+        </Button>
+      </SideBar>
+    </CartContainer>
+  )
+}
 
 export default Cart
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,3 +1,7 @@
+import { useDispatch } from 'react-redux'
+
+import { add, open } from '../../store/reducers/cart'
+
 import { Game } from '../../pages/Home'
 import { formataPreço } from '../ProductsList'
 
@@ -10,35 +14,47 @@ type Props = {
   game: Game
 }
 
-const Hero = ({ game }: Props) => (
-  <Banner style={{ backgroundImage: `url(${game.media.cover})` }}>
-    <div className="container">
-      <div>
-        <Tag>{game.details.category}</Tag>
-        <Tag>{game.details.system}</Tag>
-      </div>
-      <Infos>
-        <h2>{game.name}</h2>
-        <p>
-          {game.prices.discount && (
-            <>
-              <s>De {formataPreço(game.prices.old)}</s> <br />
-            </>
+const Hero = ({ game }: Props) => {
+  const dispatch = useDispatch()
+
+  const addToCart = () => {
+    dispatch(add(game))
+    dispatch(open())
+  }
+
+  return (
+    <Banner style={{ backgroundImage: `url(${game.media.cover})` }}>
+      <div className="container">
+        <div>
+          <Tag>{game.details.category}</Tag>
+          <Tag>{game.details.system}</Tag>
+        </div>
+        <Infos>
+          <h2>{game.name}</h2>
+          <p>
+            {game.prices.discount && (
+              <>
+                <s>De {formataPreço(game.prices.old)}</s> <br />
+              </>
+            )}
+            {game.prices.current && (
+              <>Por {formataPreço(game.prices.current)}</>
+            )}
+          </p>
+          {game.prices.current && (
+            <Button
+              variant="primary"
+              type={'button'}
+              title={'Clique aqui para adicionar esse jogo ao carrinho'}
+              onClick={addToCart}
+            >
+              Adicionar ao carrinho
+            </Button>
           )}
-          {game.prices.current && <>Por {formataPreço(game.prices.current)}</>}
-        </p>
-        {game.prices.current && (
-          <Button
-            variant="primary"
-            type={'button'}
-            title={'Clique aqui para adicionar esse jogo ao carrinho'}
-          >
-            Adicionar ao carrinho
-          </Button>
-        )}
-      </Infos>
-    </div>
-  </Banner>
-)
+        </Infos>
+      </div>
+    </Banner>
+  )
+}
 
 export default Hero
